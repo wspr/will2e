6b0b9fd2db9d73546973e336be1005fe57b3ecda
Will Robertson <wspr81@gmail.com>
2017-10-29 20:31:53 +1030

    dtxgallery

---
+++ dtxgallery/ctan.sh
@@ -0,0 +1,6 @@
+
+rm dtxgallery.zip
+zip dtxgallery.zip dtxgallery.dtx dtxgallery.pdf README
+zip dtxgallery.zip single-source.dtx single-source.pdf 
+zip dtxgallery.zip conditional-code.dtx conditional-code.pdf
+zip dtxgallery.zip rearrange.dtx rearrange.pdf
