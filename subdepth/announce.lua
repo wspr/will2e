798e522e2fd13461c70fbfe9e90f1ef96cf778c3
Will Robertson <wspr81@gmail.com>
2019-01-09 16:28:41 +0800

    subdepth: announcements ready

--- subdepth/announce.lua
+++ subdepth/announce.lua
@@ -2,9 +2,13 @@
 announce = {}
 
 announce["0.1"] = [[
-  Initial version.
+  * Initial version.
 ]]
 
 announce["0.2"] = [[
-
+  * Reworked algorithms to provide better placements in general; the original algorithm
+    gave a little too much space between subscript and superscript.
+  * Package option `low-sup` now works far more reliably/usefully.
+  * New package option `high-sup` for those who value consistency more than beauty.
+  * Fine-tuning final placements now possible with `\subdepth{...}` keyval options.
 ]]
