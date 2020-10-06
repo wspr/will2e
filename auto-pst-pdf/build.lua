4b8f1fcc5206f0f5c3cc4351308ff91817056d9b
Will Robertson <wspr81@gmail.com>
2020-10-07 00:45:35 +1030

    auto-pst-pdf build file

--- auto-pst-pdf/build.lua
+++ auto-pst-pdf/build.lua
@@ -1,5 +1,31 @@
 #!/usr/bin/env texlua
 
+--[=================================[
+     L3BUILD FILE FOR AUTO-PST-PDF
+--]=================================]
+
 module = "auto-pst-pdf"
+version = "0.7"
+copyrightyear = "2007"
 
 demofiles = {"example.tex","example.eps","example-psfrag.tex"}
+
+uploadconfig = {
+  version     = version,
+  author      = "Will Robertson",
+  license     = "lppl1.3c",
+  summary     = "Wrapper for pst-pdf",
+  ctanPath    = "/macros/latex/contrib/auto-pst-pdf",
+  repository  = "https://github.com/wspr/will2e/",
+  bugtracker  = "https://github.com/wspr/will2e/issues",
+}
+
+announce = {}
+announce["0.7"] = [[
+  * Security fix for Windows
+]]
+uploadconfig.announcement = announce[version]
+
+tagfiles = { "*.dtx" , "*.ins" , "README.txt" }
+
+require("../build-will2e.lua")
